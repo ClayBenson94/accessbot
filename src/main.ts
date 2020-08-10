2df8bc26500d86181f9d6f64dc0d16bb5fff9b6c
Brian King <brian.king@adhoc.team>
2020-08-10 19:29:46 -0400

    update main.ts

---
+++ src/main.ts
@@ -0,0 +1,32 @@
+import {App} from '@slack/bolt';
+import {newRelicLinks, md} from './nr';
+
+async function main() {
+  const app = new App({
+    token: process.env.SLACK_BOT_TOKEN,
+    signingSecret: process.env.SLACK_SIGNING_SECRET,
+  });
+
+  app.message(/^(accessbot|accessbot help)$/, async ({message, say}) => {
+    const help = `:teacher: *Accessbot subcommands*
+- *new relic*: prints new relic links
+- *help*: prints help text
+
+Make a PR: https://github.com/kingishb/accessbot
+`;
+    await say(help);
+  });
+
+  app.message(/^accessbot new relic$/, async ({context, say}) => {
+    const links = newRelicLinks();
+    const txt = md(links);
+    await say(txt);
+  });
+
+  // Start your app
+  await app.start(process.env.PORT || 3000);
+
+  console.log('⚡️ Bolt app is running!');
+}
+
+main();
